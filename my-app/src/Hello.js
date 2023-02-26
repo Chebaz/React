e27e6bf3391bf71ae3e2bdb2dc230a3fb0e9ad28
Simone Checco <simone.checco1995@gmail.com>
2023-02-26 18:18:38 +0100

    esercizio components 06

--- my-app/src/Hello.js
+++ my-app/src/Hello.js
@@ -1,6 +1,11 @@
 import React from "react";
 export default class HelloWorld extends React.Component {
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <p>What a beautiful day</p>
+      </div>
+    );
   }
 }
