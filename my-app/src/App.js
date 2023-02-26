8dc669c06594281533f9674b694c07b08b188d3b
Simone Checco <simone.checco1995@gmail.com>
2023-02-26 18:09:11 +0100

    esercizo components 03

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import HelloWorld from "./Hello";
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <HelloWorld />
+      </div>
+    );
+  }
+}
