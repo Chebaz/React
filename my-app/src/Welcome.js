fe90f6fbce789a9b6c34d71c282a2fe9f062957c
Simone Checco <simone.checco1995@gmail.com>
2023-03-05 12:41:24 +0100

    esercizio completo

--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -1,14 +1,15 @@
 import React from "react";
+import Age from "./Age";
 
 export default class Welcome extends React.Component{
     render(){
         return( 
         <div>
             <p>Welcome {this.props.nome}</p>
-            <p>Your age is {this.props.age}</p>
+            <Age age={this.props.age}/>
         </div>)
     }
 }
 Welcome.defaultProps = {
-    nome: "Marco"
+    nome: "Marco",
 };
