e7cd1b68da9cdcc0c4b0eb92089020218550d371
Simone Checco <simone.checco1995@gmail.com>
2023-03-05 11:39:13 +0100

    esercizio completato

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import HelloWorld from "./Hello";
+import Welcome from "./Welcome";
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <HelloWorld />
+        <Welcome name="Simone" />
       </div>
     );
   }
