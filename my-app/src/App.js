197da2b5b35e2badaa89599abb963162360a824c
Simone Checco <simone.checco1995@gmail.com>
2023-03-12 19:10:53 +0100

    esercizio completo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HelloWorld from "./Hello";
 import Welcome from "./Welcome";
+import Counter from "./Counter";
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <HelloWorld />
         <Welcome nome="John" age="14" />
+        <Counter />
       </div>
     );
   }
