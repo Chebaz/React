c21278c5f6a47e6c83e00b9a6c0569e2f762b3ba
Simone Checco <simone.checco1995@gmail.com>
2023-03-21 22:09:43 +0100

    esercizio completo

--- my-app/src/TodoList.js
+++ my-app/src/TodoList.js
@@ -1,37 +1,43 @@
 import React from "react";
-let array = ["esercizi", "checkpoint", "calcio", "studio"];
 export default class TodoList extends React.Component {
   state = {
     stuff: "",
+    array: ["esercizi", "checkpoint", "calcio", "studio"],
   };
-  handleInputChenge = (elem) => {
+  handleInputChange = (elem) => {
     const add = elem.target.value;
     this.setState({
       stuff: add,
     });
   };
   addThing = () => {
-    array.push(this.state.stuff);
+    this.state.array.push(this.state.stuff);
     this.setState({
       stuff: "",
     });
-    console.log(array);
+  };
+  reset = () => {
+    this.state.array.splice(0, this.state.array.length);
+    this.setState({
+      array: [],
+    });
   };
   render() {
     return (
       <div>
         <ul>
-          {array.map((item) => (
-            <li>{item}</li>
+          {this.state.array.map((item, index) => (
+            <li key={index}>{item}</li>
           ))}
         </ul>
         <input
           name="stuff"
           type="text"
           value={this.state.stuff}
-          onChange={this.handleInputChenge}
+          onChange={this.handleInputChange}
         />
         <button onClick={this.addThing}>Add</button>
+        <button onClick={this.reset}>Reset</button>
       </div>
     );
   }
