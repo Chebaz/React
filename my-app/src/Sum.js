91d78b96268e4672c534b21584d8ea01608bc514
Simone Checco <simone.checco1995@gmail.com>
2023-03-23 16:32:36 +0100

    esercizio completo

--- my-app/src/Sum.js
+++ my-app/src/Sum.js
@@ -10,3 +10,8 @@ export function Sum(props) {
   };
   return <h1>{arraySum(props.array)}</h1>;
 }
+
+Sum.defaultProps = {
+  array: [1],
+};
+/**Se non ci sono props assegnate e neanche defaultProps dichiarate le props avranno come valore null */
