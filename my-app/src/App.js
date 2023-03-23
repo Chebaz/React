da196a6234e1965d34dea4833343ea309eb84558
Simone Checco <simone.checco1995@gmail.com>
2023-03-23 18:06:30 +0100

    esercizio completo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Welcome } from "./Welcome";
 import { Sum } from "./Sum";
+import { Counter } from "./Counter";
 export default class HelloWorld extends React.Component {
   render() {
     return (
       <div>
         <Welcome />
         <Sum array={[1, 2, 3, 4, 5, 6]} />
+        <Counter />
       </div>
     );
   }
